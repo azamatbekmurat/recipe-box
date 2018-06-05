f905a62684b14dc1f2790a7444095cd470b786a7
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-06-05 13:51:48 -0700

    add app to display recipes using Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
+import { Ingredient } from './models/ingredient.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Recipe Box';
+  currentFocus: string = 'Recipe Box for cooking';
+
+  recipes: Recipe[] = [
+    new Recipe('Steak',
+            [
+              new Ingredient('Meat'),
+              new Ingredient('Onion'),
+              new Ingredient('Flour'),
+              new Ingredient('Potato'),
+              new Ingredient('Garlic')
+            ],
+            'Sprinkle salt and pepper evenly over steaks. Heat a large cast-iron skillet over high heat. Add oil to pan; swirl to coat. Add steaks to pan; cook 3 minutes on each side or until browned. Reduce heat to medium-low; add butter, thyme, and garlic to pan. Carefully grasp pan handle using an oven mitt or folded dish towel. Tilt pan toward you so butter pools; cook 1 1/2 minutes, basting steaks with butter constantly. Remove steaks from pan; cover loosely with foil. Let stand 10 minutes. Reserve butter mixture'
+    ),
+    new Recipe('Cake',
+            [
+              new Ingredient('Flour'),
+              new Ingredient('Sugar'),
+              new Ingredient('Powder'),
+              new Ingredient('Salt'),
+              new Ingredient('Milk')
+            ],
+            'Heat the oven to 180C/350F/Gas 4. Line two 18cm/7in cake tins with baking parchment'
+    )
+  ];
+
 }
---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,7 @@
+export class Recipe {
+  constructor(
+    public title: string,
+    public ingredients: object[],
+    public directions: string)
+    { }
+}
